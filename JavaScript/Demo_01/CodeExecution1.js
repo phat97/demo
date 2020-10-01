9b98225ee96285deb31d7742eaa1576642e68fda
phat97 <phat.le@hotmail.com>
2020-10-01 10:03:16 -0700

    Initial demo

---
+++ JavaScript/Demo_01/CodeExecution1.js
@@ -0,0 +1,8 @@
+/**
+ * JavaScript is a single-threaded, Async language
+ *
+ * Stack: contains the function pointers to get executed in order of Last in First out (LIFO)
+ *
+ */
+
+console.log("test");
