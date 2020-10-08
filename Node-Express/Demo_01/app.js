382dbb3bdc59bc8c75e5ba5969bab95e0a2b9582
phat97 <phat.le@hotmail.com>
2020-10-07 17:30:32 -0700

    Added more complex middleware examples

--- Node-Express/Demo_01/app.js
+++ Node-Express/Demo_01/app.js
@@ -1,14 +1,43 @@
 const express = require("express");
-const logger = require("./myLogger");
-const requestTime = require("./requestTime");
+const logger = require("./middleware/myLogger");
+const requestTime = require("./middleware/requestTime");
 const userRouter = require("./Router/users");
 const app = express();
 
 app.use(logger);
 app.use(requestTime);
 
-app.get("/", (req, res) => {
-  res.status(200).send(`The current time is: ${req.requestTime}`);
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log(`Request URL: ${req.originalUrl}`);
+    next();
+  },
+  (req, res, next) => {
+    console.log(`Request Type: ${req.method}`);
+    next();
+  },
+  (req, res) => {
+    res.status(200).send(`The current time is: ${req.requestTime}`);
+  }
+);
+
+app.get(
+  "/gamer/:id",
+  (req, res, next) => {
+    if (req.params.id === "1337") {
+      next("route");
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    res.status(200).send("Pleb");
+  }
+);
+
+app.get("/gamer/:id", (req, res) => {
+  res.status(200).send("ELITE");
 });
 
 app.use("/api/", userRouter);
